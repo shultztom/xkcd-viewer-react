c93393a2c60ecf0577393d34811d63ea182d2073
shultztom <shultzthomask@sau.edu>
2018-11-08 18:45:05 -0600

    Updates

---
+++ precache-manifest.3ab6c0cdeeca6da483dd542f39a85452.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a64a234e449210b31ba8",
+    "url": "/xkcd-viewer-react/static/js/runtime~main.a64a234e.js"
+  },
+  {
+    "revision": "708e951c3a37c05ffba5",
+    "url": "/xkcd-viewer-react/static/js/main.708e951c.chunk.js"
+  },
+  {
+    "revision": "79e0f5de928bcca9f7da",
+    "url": "/xkcd-viewer-react/static/js/1.79e0f5de.chunk.js"
+  },
+  {
+    "revision": "708e951c3a37c05ffba5",
+    "url": "/xkcd-viewer-react/static/css/main.e0d7a32f.chunk.css"
+  },
+  {
+    "revision": "79e0f5de928bcca9f7da",
+    "url": "/xkcd-viewer-react/static/css/1.09704bad.chunk.css"
+  },
+  {
+    "revision": "219f484d398bedadb705a0c080dd1049",
+    "url": "/xkcd-viewer-react/index.html"
+  }
+];

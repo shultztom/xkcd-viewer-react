6d32d4dd9793fc016a566c1fb7bb40c908a1993b
shultztom <shultzthomask@sau.edu>
2019-05-19 18:14:01 +0000

    Updates

---
+++ precache-manifest.e05b8d3c24eb4afa72b78a39d09f18ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68d753fe29f14be5d399842c8685b708",
+    "url": "/xkcd-viewer-react/index.html"
+  },
+  {
+    "revision": "43fb76cea219779c3e4b",
+    "url": "/xkcd-viewer-react/static/css/2.4806e67a.chunk.css"
+  },
+  {
+    "revision": "a6bdaf23691e3e5533ee",
+    "url": "/xkcd-viewer-react/static/css/main.948880c4.chunk.css"
+  },
+  {
+    "revision": "43fb76cea219779c3e4b",
+    "url": "/xkcd-viewer-react/static/js/2.0c5bc2cc.chunk.js"
+  },
+  {
+    "revision": "a6bdaf23691e3e5533ee",
+    "url": "/xkcd-viewer-react/static/js/main.b855bf68.chunk.js"
+  },
+  {
+    "revision": "5964a2f355ee406cd0e8",
+    "url": "/xkcd-viewer-react/static/js/runtime~main.6606971a.js"
+  }
+]);
